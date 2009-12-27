12e457769384923931d0658c0f81092ab5b9a488
Iustin Pop <iusty@k1024.org>
2009-12-27 16:16:05 +0100

    Import the PyInt_* defines from 3.0's intobject.h
    This is a stopgap measure to get the code to work under python 3.x.

--- acl.c
+++ acl.c
@@ -34,6 +34,17 @@
 
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3K
+#define PyInt_Check(op) PyLong_Check(op)
+#define PyInt_FromString PyLong_FromString
+#define PyInt_FromUnicode PyLong_FromUnicode
+#define PyInt_FromLong PyLong_FromLong
+#define PyInt_FromSize_t PyLong_FromSize_t
+#define PyInt_FromSsize_t PyLong_FromSsize_t
+#define PyInt_AsLong PyLong_AsLong
+#define PyInt_AsSsize_t PyLong_AsSsize_t
+#define PyInt_AsUnsignedLongMask PyLong_AsUnsignedLongMask
+#define PyInt_AsUnsignedLongLongMask PyLong_AsUnsignedLongLongMask
+#define PyInt_AS_LONG PyLong_AS_LONG
 #else
 #define PyBytes_Check PyString_Check
 #define PyBytes_AS_STRING PyString_AS_STRING
